e734ddb26527a78dde1c8ad51124e44f94bf27db
Yogesh Rai <yogesrai978@gmail.com>
2025-07-22 18:12:35 +0545

    admin & role based test

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import ngoRoutes from './routes/ngoRoutes.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/ngos', ngoRoutes);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log('Server running...');
+});
